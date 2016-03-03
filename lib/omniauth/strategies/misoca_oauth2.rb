dfff93c0e46a1509c18971c87f2cbc1c2fa433c1
Takafumi Yamamoto <bino98@gmail.com>
2016-03-03 14:31:38 +0900

    add errror class

--- lib/omniauth/strategies/misoca_oauth2.rb
+++ lib/omniauth/strategies/misoca_oauth2.rb
@@ -72,6 +72,20 @@ module OmniAuth
         end
         hash
       end
+
+      class CallbackError < StandardError
+        attr_accessor :error, :error_reason, :error_uri
+
+        def initialize(error, error_reason = nil, error_uri = nil)
+          self.error = error
+          self.error_reason = error_reason
+          self.error_uri = error_uri
+        end
+
+        def message
+          [error, error_reason, error_uri].compact.join(" | ")
+        end
+      end
     end
   end
 end
